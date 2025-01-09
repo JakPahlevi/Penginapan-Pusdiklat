5ac24bb19275b9fc2b94f64a5ade3c105c4a6e71
royanrosyad85 <royan.rosyad21@mhs.uinjkt.ac.id>
2025-01-09 11:16:14 +0700

    add routing user page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\BoardingHouseController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CityController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/check-booking', [BookingController::class, 'check'])->name('check-booking');
+
+Route::get('/find-hotel', [BoardingHouseController::class, 'find'])->name('find-hotel');
+Route::get('/find-results', [BoardingHouseController::class, 'findResults'])->name('find-hotel.results');
+
+Route::get('/category/{slug}', [CategoryController::class, 'show'])->name('category.show');
+Route::get('/city/{slug}', [CityController::class, 'show'])->name('city.show');
+Route::get('/hotel/{slug}', [BoardingHouseController::class, 'show'])->name('hotel.show');
+Route::get('/hotel/{slug}/rooms', [BoardingHouseController::class, 'rooms'])->name('hotel.rooms');
+
+Route::get('/hotel/booking/{slug}/information', [BookingController::class, 'information'])->name('booking.information');
+Route::post('/hotel/booking/{slug}/information/save', [BookingController::class, 'saveInformation'])->name('booking.information.save');
+Route::get('/hotel/booking/{slug}/', [BookingController::class, 'booking'])->name('booking');
+
+Route::get('/hotel/booking/{slug}/checkout', [BookingController::class, 'checkout'])->name('booking.checkout');
+Route::post('/hotel/booking/{slug}/payment', [BookingController::class, 'payment'])->name('booking.payment');
